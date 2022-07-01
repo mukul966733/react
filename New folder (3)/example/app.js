31cb822dfcb565d768c2c6eb13f3125c28fc30ca
mukul966733 <108336598+mukul966733@users.noreply.github.com>
2022-07-01 11:16:01 +0530

    Add files via upload

---
+++ New folder (3)/example/app.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import MultiStep from './index'
+import StepOne from './stepOne'
+import StepTwo from './stepTwo'
+import StepThree from './stepThree'
+import StepFour from './stepFour'
+
+const steps = [
+  { component: <StepOne /> },
+  { component: <StepTwo /> },
+  { component: <StepThree /> },
+  { component: <StepFour /> }
+]
+
+// custom styles
+const prevStyle = { background: '#33c3f0' }
+const nextStyle = { background: '#33c3f0' }
+
+const App = () => (
+  <div className='container'>
+    <MultiStep activeStep={0} steps={steps} prevStyle={prevStyle} nextStyle={nextStyle} />
+    <div className='app-footer'>
+      <h6>Press 'Enter' or click on progress bar for next step.</h6>
+      Code is on{' '}
+      <a href='' target='_blank' rel='noreferrer'>
+        github
+      </a>
+    </div>
+  </div>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'))
